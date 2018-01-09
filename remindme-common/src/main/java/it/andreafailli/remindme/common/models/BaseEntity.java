63a16a28351deadef695362c3df1c98b20a9aeb0
Andrea Failli <andry.failli@gmail.com>
2018-01-09 12:32:22 +0100

    refactored BaseEntity.equals() implementation

--- remindme-common/src/main/java/it/andreafailli/remindme/common/models/BaseEntity.java
+++ remindme-common/src/main/java/it/andreafailli/remindme/common/models/BaseEntity.java
@@ -26,7 +26,7 @@ public abstract class BaseEntity {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
 		return result;
 	}
 
@@ -39,12 +39,9 @@ public abstract class BaseEntity {
 		if (!(obj instanceof BaseEntity))
 			return false;
 		BaseEntity other = (BaseEntity) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
+		if (this.id == null) {
 			return false;
-		return true;
+		} else return this.id.equals(other.getId());
 	}
 	
 	@Override
