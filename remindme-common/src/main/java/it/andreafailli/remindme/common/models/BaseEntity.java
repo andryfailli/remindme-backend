60a64af352475a3b6e2a21fd2b77a8bde97b9a26
Andrea Failli <andry.failli@gmail.com>
2018-01-09 12:09:41 +0100

    equals & hashCode eclipse auto-generated implementation

--- remindme-common/src/main/java/it/andreafailli/remindme/common/models/BaseEntity.java
+++ remindme-common/src/main/java/it/andreafailli/remindme/common/models/BaseEntity.java
@@ -1,6 +1,5 @@
 package it.andreafailli.remindme.common.models;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.data.annotation.Id;
 
@@ -24,19 +23,28 @@ public abstract class BaseEntity {
 	}	
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
+		if (this == obj)
 			return true;
-		}
-		if (obj == null) {
+		if (obj == null)
 			return false;
-		}
-		if (!(obj instanceof BaseEntity)) {
+		if (!(obj instanceof BaseEntity))
 			return false;
-		}
 		BaseEntity other = (BaseEntity) obj;
-		return StringUtils.equals(this.getId(), other.getId());
-		
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	@Override
