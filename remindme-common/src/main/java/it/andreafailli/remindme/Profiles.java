ebe6e0feb272cb18a6b65bc9c60c475b215d7656
Andrea Failli <andry.failli@gmail.com>
2018-01-18 16:28:31 +0100

    implemented spring profiles

---
+++ remindme-common/src/main/java/it/andreafailli/remindme/Profiles.java
@@ -0,0 +1,7 @@
+package it.andreafailli.remindme;
+
+public class Profiles {
+    public static final String DEVELOPMENT = "DEVELOPMENT";
+    public static final String TEST = "TEST";
+    public static final String PRODUCTION = "PRODUCTION";
+}
