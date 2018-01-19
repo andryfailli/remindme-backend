9f10ba400651874f51c9e11009b8694ede72005b
Andrea Failli <andry.failli@gmail.com>
2018-01-19 19:24:47 +0100

    hotfix on initFirebase

--- remindme-api/src/main/java/it/andreafailli/remindme/RemindMeApiApplication.java
+++ remindme-api/src/main/java/it/andreafailli/remindme/RemindMeApiApplication.java
@@ -1,11 +1,10 @@
 package it.andreafailli.remindme;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import com.google.auth.oauth2.GoogleCredentials;
@@ -21,13 +20,13 @@ public class RemindMeApiApplication {
 		SpringApplication.run(RemindMeApiApplication.class, args);
 	}
 	
-	private static void initFirebase() throws IOException {
-		FileInputStream serviceAccount = new FileInputStream(new ClassPathResource("firebase-server-key.json").getFile());
-
+	public static void initFirebase() throws IOException {
+    	InputStream inputStream = RemindMeApiApplication.class.getClassLoader().getResourceAsStream("firebase-server-key.json");
 		FirebaseOptions options = new FirebaseOptions.Builder()
-		  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+		  .setCredentials(GoogleCredentials.fromStream(inputStream))
 		  .setDatabaseUrl("https://glass-crossing.firebaseio.com")
 		  .build();
 		if (FirebaseApp.getApps().isEmpty()) FirebaseApp.initializeApp(options);
-	}
+ 
+    }
 }
