bbcd8a0b6518847c79c4790c07b92d09f0840338
Andrea Failli <andry.failli@gmail.com>
2018-01-26 10:23:03 +0100

    added missing test category annotation

--- remindme-api/src/test/java/it/andreafailli/remindme/RemindMeApiApplicationMainIT.java
+++ remindme-api/src/test/java/it/andreafailli/remindme/RemindMeApiApplicationMainIT.java
@@ -3,7 +3,11 @@ package it.andreafailli.remindme;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+import it.andreafailli.remindme.testing.IntegrationTestCategory;
+
+@Category(IntegrationTestCategory.class)
 public class RemindMeApiApplicationMainIT {
 
 	@Test
--- remindme-common/src/test/java/it/andreafailli/remindme/RemindMeCommonTestApplicationMainIT.java
+++ remindme-common/src/test/java/it/andreafailli/remindme/RemindMeCommonTestApplicationMainIT.java
@@ -1,7 +1,11 @@
 package it.andreafailli.remindme;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+import it.andreafailli.remindme.testing.IntegrationTestCategory;
+
+@Category(IntegrationTestCategory.class)
 public class RemindMeCommonTestApplicationMainIT {
 
 	@Test
