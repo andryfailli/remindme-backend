e8ffb67dfd53d9bcbe1f0482a26d6eb9fdf0ea72
Andrea Failli <andry.failli@gmail.com>
2018-01-26 10:05:23 +0100

    refactoring exception handling

--- remindme-api/src/test/java/it/andreafailli/remindme/auth/FirebaseTokenBuilder.java
+++ remindme-api/src/test/java/it/andreafailli/remindme/auth/FirebaseTokenBuilder.java
@@ -36,41 +36,37 @@ public class FirebaseTokenBuilder {
 		return this;
 	}
 	
-	public FirebaseToken build() {
-		try {
-			Class<?> firebaseTokenImplPayloadClass = Class.forName("com.google.firebase.auth.FirebaseToken$FirebaseTokenImpl$Payload");
-			Constructor<?> firebaseTokenImplPayloadConstructor = firebaseTokenImplPayloadClass.getDeclaredConstructors()[0];
-			firebaseTokenImplPayloadConstructor.setAccessible(true);
-			Object firebaseTokenImplPayload = firebaseTokenImplPayloadConstructor.newInstance();
-			
-			Class<?> jsonWebTokenPayloadClass = Class.forName("com.google.api.client.json.webtoken.JsonWebToken$Payload");
-			Method jsonWebTokenPayloadSetSubjectMethod = jsonWebTokenPayloadClass.getMethod("setSubject", String.class);
-			jsonWebTokenPayloadSetSubjectMethod.setAccessible(true);
-			jsonWebTokenPayloadSetSubjectMethod.invoke(firebaseTokenImplPayload, this.uuid);
-			
-			Field firebaseTokenImplPayloadFieldName = firebaseTokenImplPayloadClass.getDeclaredField("name");
-			firebaseTokenImplPayloadFieldName.setAccessible(true);
-			firebaseTokenImplPayloadFieldName.set(firebaseTokenImplPayload, this.name);
-			
-			Field firebaseTokenImplPayloadFieldPicture = firebaseTokenImplPayloadClass.getDeclaredField("picture");
-			firebaseTokenImplPayloadFieldPicture.setAccessible(true);
-			firebaseTokenImplPayloadFieldPicture.set(firebaseTokenImplPayload, this.picture);
-			
-			Field firebaseTokenImplPayloadFieldEmail = firebaseTokenImplPayloadClass.getDeclaredField("email");
-			firebaseTokenImplPayloadFieldEmail.setAccessible(true);
-			firebaseTokenImplPayloadFieldEmail.set(firebaseTokenImplPayload, this.email);
-			
-			Class<?> firebaseTokenImplClass = Class.forName("com.google.firebase.auth.FirebaseToken$FirebaseTokenImpl");
-			Constructor<?> firebaseTokenImplConstructor = firebaseTokenImplClass.getDeclaredConstructors()[0];
-			firebaseTokenImplConstructor.setAccessible(true);
-			Object firebaseTokenImpl = firebaseTokenImplConstructor.newInstance(new com.google.api.client.json.webtoken.JsonWebSignature.Header(), firebaseTokenImplPayload, new byte[] {}, new byte[] {});
-			
-			Constructor<?> firebaseTokenConstructor = FirebaseToken.class.getDeclaredConstructors()[0];
-			firebaseTokenConstructor.setAccessible(true);
-			return (FirebaseToken) firebaseTokenConstructor.newInstance(firebaseTokenImpl);
-		} catch (Exception e){
-			throw new RuntimeException(e);
-		}
+	public FirebaseToken build() throws Exception {
+		Class<?> firebaseTokenImplPayloadClass = Class.forName("com.google.firebase.auth.FirebaseToken$FirebaseTokenImpl$Payload");
+		Constructor<?> firebaseTokenImplPayloadConstructor = firebaseTokenImplPayloadClass.getDeclaredConstructors()[0];
+		firebaseTokenImplPayloadConstructor.setAccessible(true);
+		Object firebaseTokenImplPayload = firebaseTokenImplPayloadConstructor.newInstance();
+		
+		Class<?> jsonWebTokenPayloadClass = Class.forName("com.google.api.client.json.webtoken.JsonWebToken$Payload");
+		Method jsonWebTokenPayloadSetSubjectMethod = jsonWebTokenPayloadClass.getMethod("setSubject", String.class);
+		jsonWebTokenPayloadSetSubjectMethod.setAccessible(true);
+		jsonWebTokenPayloadSetSubjectMethod.invoke(firebaseTokenImplPayload, this.uuid);
+		
+		Field firebaseTokenImplPayloadFieldName = firebaseTokenImplPayloadClass.getDeclaredField("name");
+		firebaseTokenImplPayloadFieldName.setAccessible(true);
+		firebaseTokenImplPayloadFieldName.set(firebaseTokenImplPayload, this.name);
+		
+		Field firebaseTokenImplPayloadFieldPicture = firebaseTokenImplPayloadClass.getDeclaredField("picture");
+		firebaseTokenImplPayloadFieldPicture.setAccessible(true);
+		firebaseTokenImplPayloadFieldPicture.set(firebaseTokenImplPayload, this.picture);
+		
+		Field firebaseTokenImplPayloadFieldEmail = firebaseTokenImplPayloadClass.getDeclaredField("email");
+		firebaseTokenImplPayloadFieldEmail.setAccessible(true);
+		firebaseTokenImplPayloadFieldEmail.set(firebaseTokenImplPayload, this.email);
+		
+		Class<?> firebaseTokenImplClass = Class.forName("com.google.firebase.auth.FirebaseToken$FirebaseTokenImpl");
+		Constructor<?> firebaseTokenImplConstructor = firebaseTokenImplClass.getDeclaredConstructors()[0];
+		firebaseTokenImplConstructor.setAccessible(true);
+		Object firebaseTokenImpl = firebaseTokenImplConstructor.newInstance(new com.google.api.client.json.webtoken.JsonWebSignature.Header(), firebaseTokenImplPayload, new byte[] {}, new byte[] {});
+		
+		Constructor<?> firebaseTokenConstructor = FirebaseToken.class.getDeclaredConstructors()[0];
+		firebaseTokenConstructor.setAccessible(true);
+		return (FirebaseToken) firebaseTokenConstructor.newInstance(firebaseTokenImpl);
 	}
 	
 }
