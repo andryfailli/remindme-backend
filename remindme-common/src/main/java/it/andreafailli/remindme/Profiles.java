9e94520e0e763617b86cd924677792dfa63cca95
Andrea Failli <andry.failli@gmail.com>
2018-02-02 11:39:26 +0100

    Revert "Profiles is now an interface"
    This reverts commit e3696dfe526b197f1fe2aba3bf18e95b66fd8ed0.

--- remindme-common/src/main/java/it/andreafailli/remindme/Profiles.java
+++ remindme-common/src/main/java/it/andreafailli/remindme/Profiles.java
@@ -1,7 +1,9 @@
 package it.andreafailli.remindme;
 
-public interface Profiles {
+public class Profiles {
     public static final String DEVELOPMENT = "DEVELOPMENT";
     public static final String TEST = "TEST";
     public static final String PRODUCTION = "PRODUCTION";
+    
+	private Profiles() {}
 }
