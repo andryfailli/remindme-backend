3eaa3d8234af315672b4e6c7d4c1463c5615aab2
Andrea Failli <andry.failli@gmail.com>
2018-02-02 12:07:11 +0100

    auto generated equals() and hashCode() removed model tests and excluded models from jacoco

--- remindme-common/src/main/java/it/andreafailli/remindme/common/models/Reminder.java
+++ remindme-common/src/main/java/it/andreafailli/remindme/common/models/Reminder.java
@@ -50,5 +50,45 @@ public class Reminder extends BaseEntity {
 	public void setArchived(boolean archived) {
 		this.archived = archived;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + (archived ? 1231 : 1237);
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof Reminder))
+			return false;
+		Reminder other = (Reminder) obj;
+		if (archived != other.archived)
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
 	
 }
--- remindme-common/src/main/java/it/andreafailli/remindme/common/models/User.java
+++ remindme-common/src/main/java/it/andreafailli/remindme/common/models/User.java
@@ -37,5 +37,42 @@ public class User extends BaseEntity {
 	public void setPhotoUrl(String photoUrl) {
 		this.photoUrl = photoUrl;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((photoUrl == null) ? 0 : photoUrl.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof User))
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (photoUrl == null) {
+			if (other.photoUrl != null)
+				return false;
+		} else if (!photoUrl.equals(other.photoUrl))
+			return false;
+		return true;
+	}
 	
 }
