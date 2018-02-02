b0721f11c9c12c3f77257776cf12d9effa45898c
Andrea Failli <andry.failli@gmail.com>
2018-02-02 12:10:10 +0100

    removed test for models in notifier app. excluded models in jacoco implemented autogenerated equals() and hashCode()

--- remindme-notifier/src/main/java/it/andreafailli/remindme/notifier/models/Notification.java
+++ remindme-notifier/src/main/java/it/andreafailli/remindme/notifier/models/Notification.java
@@ -26,6 +26,37 @@ public class Notification {
 		public void setClickAction(String clickAction) {
 			this.clickAction = clickAction;
 		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((clickAction == null) ? 0 : clickAction.hashCode());
+			result = prime * result + ((title == null) ? 0 : title.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (!(obj instanceof Data))
+				return false;
+			Data other = (Data) obj;
+			if (clickAction == null) {
+				if (other.clickAction != null)
+					return false;
+			} else if (!clickAction.equals(other.clickAction))
+				return false;
+			if (title == null) {
+				if (other.title != null)
+					return false;
+			} else if (!title.equals(other.title))
+				return false;
+			return true;
+		}
 		
 	}
 
@@ -47,6 +78,37 @@ public class Notification {
 
 	public void setData(Data data) {
 		this.data = data;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((to == null) ? 0 : to.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Notification))
+			return false;
+		Notification other = (Notification) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (to == null) {
+			if (other.to != null)
+				return false;
+		} else if (!to.equals(other.to))
+			return false;
+		return true;
 	}
 	
 }
