906189fc35f8cc869ec15f13efa0657f5f6c5adb
Andrea Failli <andry.failli@gmail.com>
2018-02-02 18:17:19 +0100

    fixed sonar minor issue

--- remindme-api/src/main/java/it/andreafailli/remindme/RemindMeApiApplication.java
+++ remindme-api/src/main/java/it/andreafailli/remindme/RemindMeApiApplication.java
@@ -31,7 +31,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class RemindMeApiApplication {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		SpringApplication.run(RemindMeApiApplication.class, args);
 	}
 	
